fb3eef8483fcbeeb571d01c66e4c06cfd7ce42d0
samiraob1997@outlook.com <samiraob1997@outlook.com>
2021-12-19 13:07:01 +0400

    Wishlist added

---
+++ Riode Solution/Riode.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
