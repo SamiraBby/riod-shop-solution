8e7246883b130a2c971dcbf13fd92e64af8d4383
samiraob1997@outlook.com <samiraob1997@outlook.com>
2021-12-19 14:14:34 +0400

    EF Core Done

---
+++ Riode Solution/Riode.WebUI/Models/DataContexts/RiodeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Riode.WebUI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.WebUI.Models.DataContexts
+{
+    public class RiodeDbContext : DbContext
+    {
+        public RiodeDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Riode Solution/Riode.WebUI/Models/Entities/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode.WebUI.Models.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? AnswerDate { get; set; }
+        public int? AnsweredByUserId { get; set; }
+
+    }
+
+}
